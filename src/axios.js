6f9a26b01fc96eb8c2b7eca96b5abd4a4784f357
laphets <i@laphets.com>
2018-04-24 20:16:28 +0800

    Change for prod

--- src/axios.js
+++ src/axios.js
@@ -11,12 +11,12 @@ const test = `https://npm.zjuqsc.com/-/verdaccio`;
 
 export default {
     get_package_list() {
-        return instance.get(`${test}/packages`);
+        return instance.get(`${root}/packages`);
     },
     get_package_detail(data) {
-        return instance.get(`${test}/package/readme/${data}`);
+        return instance.get(`${root}/package/readme/${data}`);
     },
     search(data) {
-        return instance.get(`${test}/search/${data}`);
+        return instance.get(`${root}/search/${data}`);
     }
 }
